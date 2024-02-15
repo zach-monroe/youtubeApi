74e0ced3d29b3ba4329b3bbd63035f64aa1ab2e5
Zach Monroe <z@localhost.localdomain>
2024-02-15 16:41:57 -0500

    data sends to EJS file.
    EJS file now receives data from index.js.  It is not yet styled or formatted correctly.
    
    TODOs are in index.js as well.

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ import bodyParser from "body-parser";
 //basic config
 const port = 3000;
 const app = express();
-const key = "YOUR API KEY GOES HERE";
-const channelId = "YOUR CHANNEL ID GOES HERE";
-const max = 12; //dictates how many videos render on the page.
-
+const params = {
+  key: "", //your api key
+  channelId: "", //your channel id
+  maxResults: 12, //max rendered videos
+  order: "date",
+  part: "snippet",
+};
 //middleware
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,8 +19,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", async (req, res) => {
   try {
     const result = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=${max}`,
+      "https://www.googleapis.com/youtube/v3/search?",
+      { params },
     );
+    res.render("index.ejs", {
+      items: result.data.items,
+    });
+    console.log(JSON.stringify(result.data));
   } catch (error) {}
 });
 //Youtube API
@@ -36,3 +44,7 @@ app.get("/", async (req, res) => {
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
 });
+
+// TODO: figure out a better way to manage api keys.
+// TODO: format data in ejs template (should render as a clickable image that redirects to the video link.)
+// TODO: style like website
